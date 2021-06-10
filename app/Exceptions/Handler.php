3654cf9895c58fb1f1a2ab0e5424fb92dff8d243
wangerxu <wangerxu@ddweilai.com>
2021-06-10 14:59:50 +0800

    test

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,14 @@
 
 namespace App\Exceptions;
 
+use App\Common\Conf\CommonConf;
+use App\Common\Conf\Error;
+use App\Services\Service;
+use App\Services\WechatServices\OpenPlatformService;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -29,6 +35,8 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
      * @param  \Exception  $exception
      * @return void
      */
@@ -46,6 +54,49 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        $error = ['code' => $exception->getCode(), 'msg' => $exception->getMessage()];
+        $logErr = $error;
+        $logErr['referer'] = $request->header('referer');
+        $logErr['uri'] = $request->getUri();
+        $logErr['file'] = $exception->getFile();
+        $logErr['line'] = $exception->getLine();
+        if ($error['code'] > 0 || $exception instanceof NotFoundHttpException) {
+            Log::warning(__CLASS__ . '.Exception', $logErr);
+        } else {
+            Log::error(__CLASS__ . '.Exception', $logErr);
+        }
+
+        if ($exception instanceof MiscServiceException) {
+            $error['desc'] = '微服务接口异常';
+            $error['msg'] = $error['msg'] ?: $error['desc'];
+        } elseif ($exception instanceof BusinessException) {
+            $error['desc'] = Error::getErrMsg($exception->getCode());
+            $error['msg'] = $error['msg'] ?: $error['desc'];
+        } else {
+            if (stripos($error['msg'], 'component_verify_ticket') !== false) {
+                OpenPlatformService::getInstance()->setComponentVerifyTicket();
+            }
+        }
+
+        if (isset($error['desc'])) {
+            if ($request->expectsJson()) {
+                return response()->json($error);
+            } else {
+                //@todo 需要根据当前环境（微信、wap、pc等），判断跳哪个页面
+                //@todo 要用重定向，不能直接渲染一个页面
+                //@todo 有些code需要跳转到登陆页/注册页
+                return response()->view('errors.notice', $error);
+            }
+        }
+        try {
+            //检查是否有错误缓存文件
+            $monitorFiles = ['Container.php', 'services.php', 'ProviderRepository.php'];
+            if (in_array(basename($logErr['file']), $monitorFiles)) {
+                @rename(base_path() . '/bootstrap/cache/services.php', base_path() . '/bootstrap/cache/services.php.error');
+            }
+        } catch (\Exception $e) {
+            Log::error('检查是否有错误缓存文件失败: ' . $e->getMessage());
+        }
         return parent::render($request, $exception);
     }
 }
